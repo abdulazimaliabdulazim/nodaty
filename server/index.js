080ce89d0c0e904daac06e5ae822bb706e439473
عبدالعظيم علي <118247254+abdulazimaliabdulazim@users.noreply.github.com>
2025-01-03 14:39:01 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const db = new Database();
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Create POST API to be able to cretate a new note
 app.post("/notes", (req, res) => {
